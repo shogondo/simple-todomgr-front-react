68811f5f7a9c655ea65d4b3e00b1b5c62d1d870a
Shou Takenaka <sasaminn@gmail.com>
2018-10-21 20:44:21 +0900

    Replace App to show dummy message

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router'
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+const store = createStore(
+    connectRouter(history)(
+        combineReducers({
+        })
+    ),
+    applyMiddleware(
+        logger,
+        thunk,
+        routerMiddleware(history)
+    )
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root'));
