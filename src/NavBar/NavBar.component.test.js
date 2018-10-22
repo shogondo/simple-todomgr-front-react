dd12c00c74b228c7db2cf007b0f582352170e705
Shou Takenaka <sasaminn@gmail.com>
2018-10-22 09:03:49 +0900

    Add test for navbar component

---
+++ src/NavBar/NavBar.component.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './NavBar.component';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NavBar />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
