cf77ad87712f0b1e419e26c4c6396bd68c19a9e6
Shou Takenaka <sasaminn@gmail.com>
2018-11-07 00:00:42 +0900

    Add test case with enzyme

--- src/Top/Top.component.test.js
+++ src/Top/Top.component.test.js
@@ -2,10 +2,17 @@ import ConnectedRouter from 'connected-react-router/lib/ConnectedRouter';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Top from './Top.component';
-
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<ConnectedRouter><Top/></ConnectedRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+// https://jestjs.io/docs/en/tutorial-react
+it('renders login button if user is not authenticated', () => {
+    const context = { router: { isActive: (a, b) => true } };
+    const wrapper = shallow(<Top/>, { context })
+    expect(wrapper.text()).toContain('A frontend of simple todo manager with React');
 });
